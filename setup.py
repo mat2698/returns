b27e65e6c5e7f906bd390b1b01230401dfc7b99e
martinmat98 <martin.mat@husky.neu.edu>
2020-02-13 20:24:09 -0500

    hi

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='returns',
     version='1.0',
-    packages=find_packages(exclude=['tests*']),
+    packages=find_packages(),
     license='MIT',
     description='An example python package',
     long_description=open('README.txt').read(),
-    url='https://github.com/mat2698/returns',
+    url='https://github.com/mat2698/returns/tree/master/returns',
     author='Bill Mills',
     author_email='myemail@example.com')
