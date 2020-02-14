c8745efecf2f86832a3515d9fc133fd73f739674
martinmat98 <martin.mat@husky.neu.edu>
2020-02-13 20:27:47 -0500

    adf

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='returns',
     license='MIT',
     description='An example python package',
     long_description=open('README.txt').read(),
-    url='https://github.com/mat2698/returns/tree/master/returns',
+    url='https://github.com/mat2698/returns',
     author='Bill Mills',
     author_email='myemail@example.com')
